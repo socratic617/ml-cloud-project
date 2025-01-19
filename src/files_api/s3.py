2d1e6f266f150c23c904e77c650f3b9e346dbc77
Erik Solis <erik.solis02135@gmail.com>
2025-01-19 05:48:30 -0500

    Add S3 file upload example with boto3-stubs type hints
    Demonstrates uploading a file to an S3 bucket using boto3.
    Includes mypy_boto3_s3 type hints for improved clarity and auto-completion.
    Handles missing boto3-stubs[s3] gracefully with a try-except block.
    Ensures type annotations (PutObjectOutputTypeDef, ResponseMetadataTypeDef) for enhanced development experience.

--- src/files_api/s3.py
+++ src/files_api/s3.py
@@ -1,13 +1,26 @@
 import boto3
+from mypy_boto3_s3 import S3Client
+
+try:
+    from mypy_boto3_s3 import S3Client
+    from mypy_boto3_s3.type_defs import(
+        PutObjectOutputTypeDef,
+        ResponseMetadataTypeDef,
+    )
+except ImportError:
+    print("boto3-stubs[s3] is not installed")
 
 BUCKET_NAME = "ml-cloud-bucket-erik"
 
-s3_client = boto3.client("s3")
+session = boto3.Session()
+s3_client: "S3Client" = session.client("s3")
 
 #write a file to the s3 Bucket with the content "Hello, World!"
-s3_client.put_object(
+response: "PutObjectOutputTypeDef" = s3_client.put_object(
     Bucket=BUCKET_NAME,
     Key="folder/hello.txt",
     Body="Hello, World!",
     ContentType="text/plain",
 )
+
+metadata : "ResponseMetadataTypeDef" = response["ResponseMetadata"]
