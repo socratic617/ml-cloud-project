4d8e152a4ae35853b98691fdac35aba404069658
Erik Solis <erik.solis02135@gmail.com>
2025-01-19 04:20:04 -0500

    Testing if s3 bucket recieves data

--- src/files_api/s3.py
+++ src/files_api/s3.py
@@ -3,3 +3,11 @@ import boto3
 BUCKET_NAME = "ml-cloud-bucket-erik"
 
 s3_client = boto3.client("s3")
+
+#write a file to the s3 Bucket with the content "Hello, World!"
+s3_client.put_object(
+    Bucket=BUCKET_NAME,
+    Key="folder/hello.txt",
+    Body="Hello, World!",
+    ContentType="text/plain"
+)
