c29026e66de3f17323f67fa312d204a68cfade71
Erik Solis <erik.solis02135@gmail.com>
2025-02-02 03:31:40 -0500

    fixed bug

--- tests/consts.py
+++ tests/consts.py
@@ -5,4 +5,4 @@ from pathlib import Path
 THIS_DIR = Path(__file__).parent
 PROJECT_DIR = (THIS_DIR / "../").resolve()
 
-TEST_BUCKET_NAME = "test-bucket-mlops-club-es"
+TEST_BUCKET_NAME = "ml-cloud-bucket-erik"
