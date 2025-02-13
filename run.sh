204db8c27809fd7b20e4b2b3db6d35dd1fcad302
Erik Solis <erik.solis02135@gmail.com>
2025-02-13 18:27:03 -0500

    created function to run script to mock calls to aws

--- run.sh
+++ run.sh
@@ -22,7 +22,36 @@ function install {
 
 # allows uvicorn to respond with requests with the latest changes to our code
 function run {
-    uvicorn files_api.main:APP --reload
+   AWS_PROFILE=cloud-course uvicorn files_api.main:APP --reload
+}
+
+# this will run the fast api app but it mocks(using moto) the calls to aws
+# start the FastAPI app, pointed at a mocked aws endpoint
+function run-mock {
+    set +e
+
+    # Start moto.server in the background on localhost:5000
+    python -m moto.server -p 5000 &
+    MOTO_PID=$!
+
+    # point the AWS CLI and boto3 to the mocked AWS server using mocked credentials
+    export AWS_ENDPOINT_URL="http://localhost:5000"
+    export AWS_SECRET_ACCESS_KEY="mock"
+    export AWS_ACCESS_KEY_ID="mock"
+    export AWS_PROFILE="mock"
+    export AWS_DEFAULT_REGION-"us-east-1"
+
+    # create a bucket called "some-s3-bucket-lesson-57" using the mocked aws server
+    aws s3 mb s3://some-bucket
+
+    # Trap EXIT signal to kill the moto.server process when uvicorn stops
+    trap 'kill $MOTO_PID' EXIT
+
+    # Set AWS endpoint URL and start FastAPI app with uvicorn in the foreground
+    uvicorn src.files_api.main:APP --reload
+
+    # Wait for the moto.server process to finish (this is optional if you want to keep it running)
+    wait $MOTO_PID
 }
 
 # run linting, formatting, and other static code quality tools
