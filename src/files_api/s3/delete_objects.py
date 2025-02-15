1ad41ebb3f1ce8048290f6df0f2bfac085dbff8d
Erik Solis <erik.solis02135@gmail.com>
2025-02-15 18:31:55 -0500

    Cleaned up code

--- src/files_api/s3/delete_objects.py
+++ src/files_api/s3/delete_objects.py
@@ -8,7 +8,11 @@ except ImportError:
     ...
 
 
-def delete_s3_object(bucket_name: str, object_key: str, s3_client: Optional["S3Client"] = None) -> None:
+def delete_s3_object(
+        bucket_name: str,
+        object_key: str,
+        s3_client: Optional["S3Client"] = None
+    ) -> None:
     """
     Delete an object from the S3 bucket.
 
@@ -16,4 +20,3 @@ def delete_s3_object(bucket_name: str, object_key: str, s3_client: Optional["S3C
     :param object_key: Key of the object to delete.
     :param s3_client: Optional S3 client to use. If not provided, a new client will be created.
     """
-    return
