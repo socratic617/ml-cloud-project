16c3b09deb8d9f669a600ce8318699e2ce970326
Erik Solis <erik.solis02135@gmail.com>
2025-02-15 18:32:58 -0500

    Cleaned up linting errors

--- tests/unit_tests/test__main.py
+++ tests/unit_tests/test__main.py
@@ -1,46 +1,55 @@
-import botocore
+# import botocore
+from typing import Generator
 import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 from src.files_api.main import APP
 
 # Constants for testing
-TEST_FILE_PATH = "test.txt"
-TEST_FILE_CONTENT = b"Hello, world!"
-TEST_FILE_CONTENT_TYPE = "text/plain"
+test_file_path = "test.txt"
+test_file_content = b"Hello, world!"
+test_file_content_type = "text/plain"
 
 
 # Fixture for FastAPI test client
 @pytest.fixture
-def client(mocked_aws) -> TestClient:  # pylint: disable=unused-argument
+# pylint: disable=unused-argument
+def client(mocked_aws: TestClient) -> Generator[TestClient]:
+    """
+        Create a generator with a TestClient object
+    """
     with TestClient(APP) as client:
         yield client
 
 
 def test__upload_file__happy_path(client: TestClient):
+    # create a file
+    test_file_path = "some/nested/file.txt"
+    test_file_path = b"some content"
+    test_file_path = "text/plain"
 
     response = client.put(
-        f"/files/{TEST_FILE_PATH}",
-        files={"file": (TEST_FILE_PATH, TEST_FILE_CONTENT, TEST_FILE_CONTENT_TYPE)},
+        f"/files/{test_file_path}",
+        files={"file": (test_file_path, test_file_content, test_file_content_type)},
     )
 
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json() == {
-        "file_path": TEST_FILE_PATH,
-        "message": f"New file uploaded at path: /{TEST_FILE_PATH}",
+        "file_path": test_file_path,
+        "message": f"New file uploaded at path: /{test_file_path}",
     }
 
     # update an existing file
     updated_content = b"updated content"
     response = client.put(
-        f"/files/{TEST_FILE_PATH}",
-        files={"file": (TEST_FILE_PATH, updated_content, TEST_FILE_CONTENT_TYPE)},
+        f"/files/{test_file_path}",
+        files={"file": (test_file_path, updated_content, test_file_content_type)},
     )
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
-        "file_path": TEST_FILE_PATH,
-        "message": f"Existing file updated at path: /{TEST_FILE_PATH}",
+        "file_path": test_file_path,
+        "message": f"Existing file updated at path: /{test_file_path}",
     }
 
 
@@ -49,7 +58,7 @@ def test_list_files_with_pagination(client: TestClient):
     for i in range(15):
         client.put(
             f"/files/file{i}.txt",
-            files={"file": (f"file{i}.txt", TEST_FILE_CONTENT, TEST_FILE_CONTENT_TYPE)},
+            files={"file": (f"file{i}.txt", test_file_content, test_file_content_type)},
         )
     # List files with page size 10
     response = client.get("/files?page_size=10")
@@ -62,39 +71,39 @@ def test_list_files_with_pagination(client: TestClient):
 def test_get_file_metadata(client: TestClient):
     # Upload a file
     client.put(
-        f"/files/{TEST_FILE_PATH}",
-        files={"file": (TEST_FILE_PATH, TEST_FILE_CONTENT, TEST_FILE_CONTENT_TYPE)},
+        f"/files/{test_file_path}",
+        files={"file": (test_file_path, test_file_content, test_file_content_type)},
     )
     # Get file metadata
-    response = client.head(f"/files/{TEST_FILE_PATH}")
+    response = client.head(f"/files/{test_file_path}")
     assert response.status_code == 200
     headers = response.headers
-    assert headers["Content-Type"] == TEST_FILE_CONTENT_TYPE
-    assert headers["Content-Length"] == str(len(TEST_FILE_CONTENT))
+    assert headers["Content-type"] == test_file_content_type
+    assert headers["Content-Length"] == str(len(test_file_content))
     assert "Last-Modified" in headers
 
 
 def test_get_file(client: TestClient):
     # Upload a file
     client.put(
-        f"/files/{TEST_FILE_PATH}",
-        files={"file": (TEST_FILE_PATH, TEST_FILE_CONTENT, TEST_FILE_CONTENT_TYPE)},
+        f"/files/{test_file_path}",
+        files={"file": (test_file_path, test_file_content, test_file_content_type)},
     )
     # Get file
-    response = client.get(f"/files/{TEST_FILE_PATH}")
+    response = client.get(f"/files/{test_file_path}")
     assert response.status_code == 200
-    assert response.content == TEST_FILE_CONTENT
+    assert response.content == test_file_content
 
 
 def test_delete_file(client: TestClient):
     # Upload a file
     client.put(
-        f"/files/{TEST_FILE_PATH}",
-        files={"file": (TEST_FILE_PATH, TEST_FILE_CONTENT, TEST_FILE_CONTENT_TYPE)},
+        f"/files/{test_file_path}",
+        files={"file": (test_file_path, test_file_content, test_file_content_type)},
     )
 
     # Delete file
-    response = client.delete(f"/files/{TEST_FILE_PATH}")
+    response = client.delete(f"/files/{test_file_path}")
     assert response.status_code == 204
 
     # Verify deletion
@@ -105,4 +114,6 @@ def test_delete_file(client: TestClient):
     #
     # Later we will fix this by doing better error handling within the API itself.
     with pytest.raises(botocore.exceptions.ClientError):
-        response = client.get(f"/files/{TEST_FILE_PATH}")
+        response = client.get(f"/files/{test_file_path}")
+
+
