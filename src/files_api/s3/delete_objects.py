d3204e867323feb62b3fee5f6cc4f5df37130849
Erik Solis <erik.solis02135@gmail.com>
2025-02-15 23:13:27 -0500

    imported boto3 + Refactor: Add optional S3 client argument to delete_s3_object, defaulting to new client if not provided

--- src/files_api/s3/delete_objects.py
+++ src/files_api/s3/delete_objects.py
@@ -2,6 +2,8 @@
 
 from typing import Optional
 
+import boto3
+
 try:
     from mypy_boto3_s3 import S3Client
 except ImportError:
@@ -20,3 +22,5 @@ def delete_s3_object(
     :param object_key: Key of the object to delete.
     :param s3_client: Optional S3 client to use. If not provided, a new client will be created.
     """
+    s3_client = s3_client or boto3.client("s3")
+    s3_client.delete_object(Bucket=bucket_name, Key=object_key)
