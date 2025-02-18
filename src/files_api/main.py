5a9522e5bd78cd64f770d41e1fd3ffa1a40e7563
Erik Solis <erik.solis02135@gmail.com>
2025-02-17 23:03:16 -0500

    Refactor app initialization and S3 bucket handling
    - Removed global `APP` variable and moved app creation into `create_app` function
    - Changed `s3_bucket_name` to lowercase for consistency across functions
    - Refactored `create_app` to return FastAPI app instance at the end

--- src/files_api/main.py
+++ src/files_api/main.py
@@ -1,6 +1,7 @@
 """
     TODO
 """
+import os
 from datetime import datetime
 from typing import (
     List,
@@ -30,14 +31,14 @@ from files_api.s3.write_objects import upload_s3_object
 # --- Constants --- #
 #####################
 
-S3_BUCKET_NAME = "some-s3-bucket-lesson-57"
+# s3_bucket_name = os.environ["some-s3-bucket-lesson-57"]
 
 # Instance of FastAPI
 # Labeled "APP" in all caps because it is a constant, defined
 #  at the global scope. Using uppercase helps prevent state-related
 # bugs and signals to others that it should be treated as a constant.
 
-APP = FastAPI()
+# app = FastAPI()
 
 ####################################
 # --- Request/response schemas --- #
@@ -64,7 +65,7 @@ class GetFilesResponse(BaseModel):
     """
         TODO
     """
-    files: List[FileMetadata]
+    files: list[FileMetadata]
     next_page_token: Optional[str]
 
 class GetFilesQueryParams(BaseModel):
@@ -83,104 +84,111 @@ class DeleteFileResponse(BaseModel):
     message: str
 
 
+
 ##################
 # --- Routes --- #
 ##################
+def create_app(s3_bucket_name: str | None = None) -> FastAPI:
+    """Create a FastAPI application."""
+    print("TESTIE")
+    s3_bucket_name = s3_bucket_name or os.environ["S3_BUCKET_NAME"]
+    app = FastAPI()
 
+    @app.put("/files/{file_path:path}")
+    async def upload_file(file_path: str, file: UploadFile, response: Response) -> PutFileResponse:
+        """Upload a file."""
 
-@APP.put("/files/{file_path:path}")
-async def upload_file(file_path: str, file: UploadFile, response: Response) -> PutFileResponse:
-    """Upload a file."""
+        file_contents: bytes = await file.read()
 
-    file_contents: bytes = await file.read()
-    object_already_exists_at_path = object_exists_in_s3(S3_BUCKET_NAME, object_key=file_path)
-    if object_already_exists_at_path:
-        message = f"Existing file updated at path: /{file_path}"
+        object_already_exists_at_path = object_exists_in_s3(s3_bucket_name, object_key=file_path)
+        if object_already_exists_at_path:
+            message = f"Existing file updated at path: /{file_path}"
+            response.status_code = status.HTTP_200_OK
+        else:
+            message = f"New file uploaded at path: /{file_path}"
+            response.status_code = status.HTTP_201_CREATED
+
+        upload_s3_object(
+            bucket_name=s3_bucket_name,
+            object_key=file_path,
+            file_content=file_contents,
+            content_type=file.content_type,
+        )
+
+        return PutFileResponse(
+            file_path=f"{file_path}",
+            message=message
+        )
+
+    @app.get("/files")
+    async def list_files(
+        query_params: GetFilesQueryParams = Depends(),  # noqa: B008
+    ) -> GetFilesResponse:
+        """List files with pagination."""
+        if query_params.page_token:
+            files, next_page_token = fetch_s3_objects_using_page_token(
+                bucket_name=s3_bucket_name,
+                continuation_token=query_params.page_token,
+                max_keys=query_params.page_size,
+            )
+        else:
+            files, next_page_token = fetch_s3_objects_metadata(
+                bucket_name=s3_bucket_name,
+                prefix=query_params.directory,
+                max_keys=query_params.page_size,
+            )
+
+        file_metadata_objs = [
+            FileMetadata(
+                file_path=f"{item['Key']}",
+                last_modified=item["LastModified"],
+                size_bytes=item["Size"],
+            )
+            for item in files
+        ]
+        return GetFilesResponse(files=file_metadata_objs, next_page_token=next_page_token if next_page_token else None)
+
+    @app.head("/files/{file_path:path}")
+    async def get_file_metadata(file_path: str, response: Response) -> Response:
+        """Retrieve file metadata.
+
+        Note: by convention, HEAD requests MUST NOT return a body in the response.
+        """
+        get_object_response = fetch_s3_object(s3_bucket_name, object_key=file_path)
+        response.headers["Content-Type"] = get_object_response["ContentType"]
+        response.headers["Content-Length"] = str(get_object_response["ContentLength"])
+        response.headers["Last-Modified"] = get_object_response["LastModified"].strftime("%a, %d %b %Y %H:%M:%S GMT")
         response.status_code = status.HTTP_200_OK
-    else:
-        message = f"New file uploaded at path: /{file_path}"
-        response.status_code = status.HTTP_201_CREATED
+        return response
 
-    upload_s3_object(
-        bucket_name=S3_BUCKET_NAME,
-        object_key=file_path,
-        file_content=file_contents,
-        content_type=file.content_type,
-    )
 
-    return PutFileResponse(
-        file_path=f"{file_path}",
-        message=message
-    )
-
-@APP.get("/files")
-async def list_files(
-    query_params: GetFilesQueryParams = Depends(),  # noqa: B008
-) -> GetFilesResponse:
-    """List files with pagination."""
-    if query_params.page_token:
-        files, next_page_token = fetch_s3_objects_using_page_token(
-            bucket_name=S3_BUCKET_NAME,
-            continuation_token=query_params.page_token,
-            max_keys=query_params.page_size,
-        )
-    else:
-        files, next_page_token = fetch_s3_objects_metadata(
-            bucket_name=S3_BUCKET_NAME,
-            prefix=query_params.directory,
-            max_keys=query_params.page_size,
+    @app.get("/files/{file_path:path}")
+    async def get_file(
+        file_path: str,
+    ) -> StreamingResponse:
+        """Retrieve a file."""
+        get_object_response = fetch_s3_object(s3_bucket_name, object_key=file_path)
+        return StreamingResponse(
+            content=get_object_response["Body"],
+            media_type=get_object_response["ContentType"],
         )
 
-    file_metadata_objs = [
-        FileMetadata(
-            file_path=f"{item['Key']}",
-            last_modified=item["LastModified"],
-            size_bytes=item["Size"],
-        )
-        for item in files
-    ]
-    return GetFilesResponse(files=file_metadata_objs, next_page_token=next_page_token if next_page_token else None)
 
-@APP.head("/files/{file_path:path}")
-async def get_file_metadata(file_path: str, response: Response) -> Response:
-    """Retrieve file metadata.
+    @app.delete("/files/{file_path:path}")
+    async def delete_file(
+        file_path: str,
+        response: Response,
+    ) -> Response:
+        """Delete a file.
 
-    Note: by convention, HEAD requests MUST NOT return a body in the response.
-    """
-    get_object_response = fetch_s3_object(S3_BUCKET_NAME, object_key=file_path)
-    response.headers["Content-Type"] = get_object_response["ContentType"]
-    response.headers["Content-Length"] = str(get_object_response["ContentLength"])
-    response.headers["Last-Modified"] = get_object_response["LastModified"].strftime("%a, %d %b %Y %H:%M:%S GMT")
-    response.status_code = status.HTTP_200_OK
-    return response
+        #NOTE: DELETE requests MUST NOT return a body in the response."""
+        delete_s3_object(s3_bucket_name, object_key=file_path)
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response
 
-
-@APP.get("/files/{file_path:path}")
-async def get_file(
-    file_path: str,
-) -> StreamingResponse:
-    """Retrieve a file."""
-    get_object_response = fetch_s3_object(S3_BUCKET_NAME, object_key=file_path)
-    return StreamingResponse(
-        content=get_object_response["Body"],
-        media_type=get_object_response["ContentType"],
-    )
-
-
-@APP.delete("/files/{file_path:path}")
-async def delete_file(
-    file_path: str,
-    response: Response,
-) -> Response:
-    """Delete a file.
-
-    NOTE: DELETE requests MUST NOT return a body in the response."""
-    delete_s3_object(S3_BUCKET_NAME, object_key=file_path)
-    response.status_code = status.HTTP_204_NO_CONTENT
-    return response
-
+    return app
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run(APP, host="0.0.0.0", port=8000)
+    app = create_app()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
