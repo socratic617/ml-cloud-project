488c4857e5be41e5b763542a2b888bb2a13ff8f7
Erik Solis <erik.solis02135@gmail.com>
2025-04-23 19:46:57 -0400

    add Pydantic BaseSettings for files API

---
+++ src/files_api/settings.py
@@ -0,0 +1,16 @@
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict
+)
+
+class Settings(BaseSettings):
+    """Settings for the files API.
+
+    Pydantic BaseSettings doc: https://docs.pydantic.dev/latest/concepts/pydantic_settings/#installation
+    FASTAPI guide to managing settings: https://fastapi.tiangolo.com/advanced/settings/
+    """
+    s3_bucket_name: str
+
+    model_config = SettingsConfigDict(
+        case_sensitive=False
+    )
