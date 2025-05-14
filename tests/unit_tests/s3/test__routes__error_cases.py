e4c855941a75bd0f2e136fdcab2ec9a7fc7cc791
Erik Solis <erik.solis02135@gmail.com>
2025-05-13 22:53:41 -0400

    Add tests for invalid page_size in GET /files endpoint

--- tests/unit_tests/s3/test__routes__error_cases.py
+++ tests/unit_tests/s3/test__routes__error_cases.py
@@ -1,5 +1,6 @@
 from fastapi import status
 from fastapi.testclient import TestClient
+from files_api.schemas import DEFAULT_GET_FILES_MAX_PAGE_SIZE
 from tests.consts import TEST_BUCKET_NAME
 
 def test_get_nonexistant_file(client:TestClient):
@@ -8,6 +9,9 @@ def test_get_nonexistant_file(client:TestClient):
     assert response.json() == {"detail": "File not found"}
 
 def test_head_nonexistent_file(client: TestClient):
+    '''
+    Test the HEAD request for a nonexistent file.
+    '''
     response = client.head("/files/nonexistent_file.txt")
     assert response.status_code == 404
 
@@ -16,3 +20,9 @@ def test_delete_nonexistent_file(client: TestClient):
     response = client.delete("/files/nonexistent_file.txt")
     assert response.status_code == 404
     assert response.json() == {"detail": "File not found"}
+
+def test_get_files_invalid_page_size(client: TestClient):
+    response = client.get("/files?page_size=-1")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    response = client.get(f'/files?page_size={DEFAULT_GET_FILES_MAX_PAGE_SIZE + 1}')
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
