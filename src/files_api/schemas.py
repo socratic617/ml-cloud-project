f1328ccd8190614e602622e3bfe3af6fd709e506
Erik Solis <erik.solis02135@gmail.com>
2025-05-16 11:04:18 -0400

    - Added @model_validator (mode='after') to ensure page_token is mutually exclusive with page_size and directory. - Updated directory default to use DEFAULT_GET_FILES_DIRECTORY instead of an empty string. - Imported model_validator from pydantic and Self from typing_extensions for compatibility with after-mode validation.

--- src/files_api/schemas.py
+++ src/files_api/schemas.py
@@ -6,11 +6,13 @@ from typing import (
     List,
     Optional,
 )
+from typing_extensions import Self
 from unittest.mock import DEFAULT
 
 from pydantic import (
     BaseModel,
     Field,
+    model_validator
 )
 
 DEFAULT_GET_FILES_PAGE_SIZE = 10
@@ -51,8 +53,18 @@ class GetFilesQueryParams(BaseModel):
         ge=DEFAULT_GET_FILES_MIN_PAGE_SIZE,
         le=DEFAULT_GET_FILES_MAX_PAGE_SIZE,
     )
-    directory: Optional[str] = ""
+    directory: Optional[str] = DEFAULT_GET_FILES_DIRECTORY
     page_token: Optional[str] = None
+
+    @model_validator(mode='after')
+    def check_passwords_match(self) -> Self:
+        if self.page_token:
+            get_files_query_params: dict = self.model_dump(exclude_unset=True)
+            page_size_set = "page_size" in get_files_query_params.keys()
+            directory_set = "directory" in get_files_query_params.keys()
+            if page_size_set or directory_set:
+                raise ValueError("page_token is mutually exclusive with page_size and directory")
+        return self
 
 # delete (cruD)
 class DeleteFileResponse(BaseModel):
