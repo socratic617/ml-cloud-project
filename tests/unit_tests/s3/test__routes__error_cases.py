6abfde8a8600ceb0580e95fa1e7a675b85fe6541
Erik Solis <erik.solis02135@gmail.com>
2025-05-16 11:08:51 -0400

    - Added test to ensure API returns 422 when page_token is used with page_size or directory. - Validates enforcement of new Pydantic model validator logic. - Confirms error message includes 'mutually exclusive' to help with debugging and clarity.

--- tests/unit_tests/s3/test__routes__error_cases.py
+++ tests/unit_tests/s3/test__routes__error_cases.py
@@ -26,3 +26,17 @@ def test_get_files_invalid_page_size(client: TestClient):
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     response = client.get(f'/files?page_size={DEFAULT_GET_FILES_MAX_PAGE_SIZE + 1}')
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+def test_get_files_page_token_is_mutually_exclusive_with_page_size_and_directory(client: TestClient):
+    response = client.get("/files?page_token=token&page_size=10")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert "mutually exclusive" in str(response.json())
+
+    response = client.get("/files?page_token=token&directory=dir")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert "mutually exclusive" in str(response.json())
+
+
+    response = client.get("/files?page_token=token&page_size=10&directory=dir")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert "mutually exclusive" in str(response.json())
